8c1a0912579e0b75defe794f70fa74c24cf3f169
Ana Lucia Bulacios <analubula87@gmail.com>
2023-01-08 03:21:55 -0300

    addstilos1

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -147,7 +147,7 @@ const rootReducer = (state = initialState, action) => {
           return {
             ...state,
        
-          }
+          }                                  
        
                               
         default:
